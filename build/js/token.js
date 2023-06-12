620f041214c93bd364d6fbfa49b1c6a7698b9ed7
raphaeldamasceno1852 <rdamasceno1852@gmail.com>
2023-06-12 19:27:44 -0300

    init: initial commit

---
+++ build/js/token.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 12 Jun 2023 22:21:28 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#a88fa8",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#09090b",
+  "ColorPurple500": "#885cf6"
+};
